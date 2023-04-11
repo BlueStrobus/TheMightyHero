886348214b7ee4afe16aefb23bb5b6cf14efd4e9
SanghooPark <126745119+iamhoooo@users.noreply.github.com>
2023-04-11 11:28:52 +0900

    첫번째 계산기파일

--- main/Calculator.py
+++ main/Calculator.py
@@ -1,1 +1,19 @@
 # 시작하세요
+def calculator():
+    num1 = float(input("첫 번째 숫자를 입력하세요: "))
+    op = input("연산자를 입력하세요 (+, -, *, /): ")
+    num2 = float(input("두 번째 숫자를 입력하세요: "))
+
+    if op == '+':
+        result = num1 + num2
+    elif op == '-':
+        result = num1 - num2
+    elif op == '*':
+        result = num1 * num2
+    elif op == '/':
+        result = num1 / num2
+    else:
+        print("잘못된 연산자입니다.")
+        return
+
+    print(f"{num1} {op} {num2} = {result}")
