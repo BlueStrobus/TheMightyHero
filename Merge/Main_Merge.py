8c14e809009b616b2974619f65bdff56f0873f9e
an yong ju <for4014@gmail.com>
2023-04-11 17:03:11 +0900

    초기화

--- Merge/Main_Merge.py
+++ Merge/Main_Merge.py
@@ -1,16 +1,1 @@
 #머지 용
-import tkinter as tk
-import random
-
-root = tk.Tk()
-root.title("간단한 RPG 게임")
-
-def create_character():
-    jobs = ["전사", "마법사", "궁수", "도적"]
-    character = {}
-    character["name"] = name_entry.get()
-    character["job"] = random.choice(jobs)
-    character["level"] = random.randint(1, 10) 
-    character["hp"] = random.randint(50, 100) * character["level"]
-    character["mp"] = random.randint(30, 50) * character["level"]
-    return character
